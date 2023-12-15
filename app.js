d9c2a0a837df9b2a731fab754dac713b8715af38
serkandyck <serkandayicik@gmail.com>
2023-12-15 14:00:59 +0300

    setting body parser for json request and add some routes mechanics

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 let app = express();
 
@@ -9,8 +10,20 @@ const PORT = process.env.PORT || 3000;
 
 // NodeJS view engine ejs
 app.set("view engine", "ejs");
+// Uygulama view klasörünü kullanıyor
+app.set("views", "./src/views");
+
+// requestler üzerindeki json verileri okuyabilmek için body-parser kullanılıyor
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Uygulama router express kütüphanesini tek yerden çağırmak için ana girişten set edilip konfigrasyon routes üzerinden yapılıyor
+const router = express.Router();
+app.use(router);
 
 
-app.listen(PORT, () => {
+// Uygulama env üzerinde belirtilen port üzerinde çalıştırılıyor
+app.listen(PORT, err => {
+    if (err) return console.log(`Cannot Listen on PORT: ${PORT}`);
     console.log(`Server running on port ${PORT}`);
 });

833c5cd2ae406bd5c5cd87a3765cb8298af8d0c6
serkandyck <serkandayicik@gmail.com>
2023-12-15 13:48:36 +0300

    env and port setup

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ let app = express();
 
 require('dotenv').config()
 
+// Env üzerinden çalıştırılan port numarasını alıyor belirtilen yoksa 3000 portunu kullanıyor
 const PORT = process.env.PORT || 3000;
+
+// NodeJS view engine ejs
+app.set("view engine", "ejs");
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

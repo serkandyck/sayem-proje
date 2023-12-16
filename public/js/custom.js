6ef7c99497ddf3bc2b8c53eaad0e229cc612d04d
serkandyck <serkandayicik@gmail.com>
2023-12-16 13:18:07 +0300

    add form validation + succ modal

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,5 @@
 $( "#request-form" ).on("submit", function( event ) {
+    
     var formData = {
         requestType: $("#request_type").val(),
         requestTitle: $("#request_title").val(),
@@ -13,11 +14,28 @@ $( "#request-form" ).on("submit", function( event ) {
         encode: true,
       }).done(function (data) {
         Swal.fire({
-            title: 'Error!',
-            text: 'Do you want to continue',
-            icon: 'error',
-            confirmButtonText: 'Cool'
+            title: data.message,
+            html: `
+              <div class="alert alert-success" role="alert">
+                Talep durumunuzu sorgulamak için talep numaraınızı kaydetmeyi unutmayın!
+              </div>
+              Talep numaranız </br><b>${data.uuid}</b>
+            `,
+            icon: 'success',
+            confirmButtonText: 'Tamam'
         })
+        document.body.classList.remove('swal2-height-auto');
+        $( "#request-form" ).trigger("reset");
+        $("#succ-alert").removeClass("hidden");
+        $("#succ-alert").html(data.message + " Talep numaranız </br><b>" + data.uuid + "</b>");
+      }).fail(function () {
+        Swal.fire({
+            title: "Beklenmedik bir hata ile karşılaşıldı",
+            icon: 'error',
+            confirmButtonText: 'Tamam'
+        })
+        $("#err-alert").removeClass("hidden");
+        $("#err-alert").html("Beklenmedik bir hata ile karşılaşıldı");
       });
 
     event.preventDefault();
--- src/controllers/requestController.js
+++ src/controllers/requestController.js
@@ -1,22 +1,32 @@
 const db = require("../config/dbContext");
+const { v4: uuidv4 } = require('uuid');
+
 
 const requestController = async(req, res) => {
     const { requestType, requestTitle, requestContent } = req.body;
     //TODO validation
 
-    const request = await db.Request.create({
-        data :{
-            title: requestTitle,
-            description: requestContent,
-            requestType : {
-                connect: {
-                    id: parseInt(requestType)
+    try {
+        const request = await db.Request.create({
+            data :{
+                uuid: uuidv4(),
+                title: requestTitle,
+                content: requestContent,
+                requestType : {
+                    connect: {
+                        id: parseInt(requestType)
+                    }
                 }
             }
-        }
-    });
+        });
+        
+        res.status(201).json({message: "Talebiniz başarıyla alındı.", uuid: request.uuid});
+    } catch (error) {
+        res.status(500).json({message: "Beklenmedik bir hata ile karşılaşıldı.",});
+    }
+    
 
-    res.redirect("/");
+    
 }
 
 module.exports =  {

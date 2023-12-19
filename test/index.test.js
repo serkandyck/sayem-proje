e74cc65b689eb89f307677207c5d7cfa5d592454
serkandyck <serkandayicik@gmail.com>
2023-12-19 23:45:39 +0300

    added test framework

---
+++ test/index.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../server/app.js');
+
+// basit test
+describe('GET /', function() {
+  it('return json response', function() {
+    return request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type',/json/)
+      .expect('{"text":"some json"}')
+  })
+})
